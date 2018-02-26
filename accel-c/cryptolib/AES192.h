8d21c85bfde480641d9ec6f90d6c6b213428c369
Double Sine <xiao_ai_yu@live.cn>
2018-02-26 20:48:51 +0800

    add left declaration.

--- accel-c/cryptolib/AES192.h
+++ accel-c/cryptolib/AES192.h
@@ -17,8 +17,9 @@ extern "C" {
     void CRYPTO_AES192_EncryptBlock_AESNI(uint8_t srcBytes[16], const uint32_t srcExpandedKey[52]);
     void CRYPTO_AES192_DecryptBlock_AESNI(uint8_t srcBytes[16], const uint32_t srcExpandedKey[52]);
     void CRYPTO_AES192_DecryptBlock_AESNI_Fast(uint8_t srcBytes[16], const uint32_t srcInvExpandedKey[52]);
-    void CRYPTO_AES192_KeyExpansion_AESNI_AVX2(const uint8_t srcKey[24], uint32_t dstExpandedKey[52]);
 
+    void CRYPTO_AES192_KeyExpansion_AESNI_AVX2(const uint8_t srcKey[24], uint32_t dstExpandedKey[52]);
+    void CRYPTO_AES192_GetInvExpandedKey_AESNI(const uint32_t srcExpandedKey[52], uint32_t dstInvExpandedKey[52]);
 #if defined(__cplusplus)
 }
 #endif
