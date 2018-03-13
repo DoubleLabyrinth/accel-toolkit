d0593ef15d884d9f10f87879df561ac9e3aca49a
Double Sine <xiao_ai_yu@live.cn>
2018-03-13 19:31:38 +0800

    Add math_uintx_bit_length

--- accel-c/mathlib/source/cmp.c
+++ accel-c/mathlib/source/cmp.c
@@ -4,6 +4,14 @@
 *********************************************************************/
 #include "../cmp.h"
 
+#if defined(_M_X64)
+#define _BitScanReverse_coeff _BitScanReverse64
+#elif defined(_M_IX86)
+#define _BitScanReverse_coeff _BitScanReverse
+#else
+#error "No matched architecture found."
+#endif
+
 int8_t math_uintx_cmp(const coeff_t src1[], size_t length1,
                       const coeff_t src2[], size_t length2) {
 
@@ -41,4 +49,16 @@ int8_t math_uintx_isZero(const coeff_t src[], size_t length) {
     for (size_t i = 0; i < length; ++i) 
         if (src[i] != 0) return 0;
     return 1;
+}
+
+size_t math_uintx_bit_length(const coeff_t src[], size_t length) {
+    const coeff_t* cur_src = src + length - 1;
+    while (cur_src != src && *cur_src == 0)
+        --cur_src;
+
+    unsigned long Index;
+    if (_BitScanReverse_coeff(&Index, *cur_src)) 
+        return (cur_src - src) * sizeof(coeff_t) * 8 + Index + 1;
+    else 
+        return 0;
 }
